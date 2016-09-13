994a29fca16803bd09e4051492d01a1fc5993fea
Fabian Latorre <latorrefabian@gmail.com>
2016-09-13 18:45:58 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='elsevier-sdk-python',
+      version='1.0',
+      description='elsevier API python client',
+      author='Fabian Latorre',
+      author_email='latorrefabian@gmail.com',
+      url='',
+      packages=['elsevier'],
+     )
