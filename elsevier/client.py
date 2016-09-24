4a21b290c545334db049b0ab616cc96a3e866215
Fabian Latorre <latorrefabian@gmail.com>
2016-09-24 01:04:33 -0500

    add new path structure. TO DO: add support for XML response

--- elsevier/client.py
+++ elsevier/client.py
@@ -1,9 +1,16 @@
 import requests
 
 from exceptions import ElsevierException
+import re
 
 
 class ElsevierClient(object):
+    methods = {
+        'search_scopus': '/search/scopus',
+        'search_science_direct': '/search/scidir',
+        'retrieve_abstract': '/abstract/{id_type}/{id}'
+    }
+
     def __init__(self,
                  api_key,
                  host='http://api.elsevier.com/content'):
@@ -11,7 +18,7 @@ class ElsevierClient(object):
         self.host = host
 
     def _request(self, endpoint, params, requestType='GET'):
-        response= None
+        response = None
         if self.api_key is not None:
             url = u'{0}{1}'.format(self.host, endpoint)
             params['apiKey'] = self.api_key
@@ -21,17 +28,29 @@ class ElsevierClient(object):
                 response = requests.get(url, params)
         else:
             raise ElsevierException('No API Key.')
+        print 'response type: ' + response.headers['content-type']
         return self._parse_response(response)
 
     def _parse_response(self, response):
         if response.status_code == 200:
-            return response.json()
+            content_type = response.headers['content-type']
+            if content_type.startswith('application/json'):
+                return response.json()
+            else:
+                raise ElsevierException('content-type ' +
+                    content_type + ' not yet supported') 
         elif response.status_code == 429:
             raise ElsevierException('Quota Exceeded.')
+    
+    def _api_method(self, endpoint):
+        def method(**kwargs):
+            endpoint_format_kwargs = re.findall('{([\w_]+)}', endpoint)
+            formatted_endpoint = endpoint.format(**kwargs)
+            for kwarg in endpoint_format_kwargs:
+                kwargs.pop(kwarg, None)
+            return self._request(formatted_endpoint, kwargs)
+        return method
 
+    def __getattr__(self, name):
+        return self._api_method(ElsevierClient.methods[name])
 
-    def search_scopus(self, **kwargs):
-        return self._request('/search/scopus', kwargs)
-
-    def search_science_direct(self, query):
-        return self._request('/search/scidir', { 'query': query })
